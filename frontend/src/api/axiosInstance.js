b491730acce3e339487fab34e8281f5b0b6937ba
Deva <devaraj167@gmail.com>
2026-07-27 08:05:57 +0530

    change backend url

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: "https://cricket-tournament-management.onrender.com/api",
     withCredentials: true
 })
 
